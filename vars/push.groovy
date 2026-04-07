eeefc61f4e60e66700f5e910828f10e5d09b07f3
MUHAMMAD ALI HASSAN <muhammadalihassan792@gmail.com>
2026-04-07 14:27:52 +0500

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,6 +1,7 @@
 def call(String imageName, String tag, String dockerHubUser){
   withCredentials([usernamePassword(credentialsId: 'jenkins', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
   sh "docker login -u ${DOCKER_USER} -p ${DOCKER_PASS}" 
+  sh "docker build -t ${dockerHubUser}/${imageName}:${tag} ."
   sh "docker push ${dockerHubUser}/${imageName}:${tag}"
   }
 }
